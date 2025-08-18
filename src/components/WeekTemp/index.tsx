18224562b370f93701a25c0dc2f3c9581954b314
CarlosGonzaga11 <carlospzk11@gmail.com>
2025-08-18 17:10:15 -0300

    consumindo api de clima e deixando os states dinamicos

--- src/components/WeekTemp/index.tsx
+++ src/components/WeekTemp/index.tsx
@@ -1,17 +1,23 @@
 import { CiTempHigh } from "react-icons/ci";
-export default function WeekTemp() {
+type WeekTempProps = {
+  min: number;
+  max: number;
+  day: string;
+  dayNumber: number;
+};
+export default function WeekTemp({ min, max, day, dayNumber }: WeekTempProps) {
   return (
     <div className="bg-[#F6F9FB] rounded flex gap-1 h-[40px] text-[12px] justify-between hover:bg-[#eff0f0] transition">
       <div className="text-[#81BDFC] flex items-center gap-2 pl-2 ">
-        <CiTempHigh className="text-[16px]"/>
+        <CiTempHigh className="text-[16px]" />
         <div className="flex items-baseline flex-col">
-          <p className="text-[#D9D9D9]">Hoje</p>
-          <p className="text-[#000]">16 ago</p>
+          <p className="text-[#4d4b4b]">{day}</p>
+          <p className="text-[#000] opacity-40">{dayNumber} ago</p>
         </div>
       </div>
       <div className="flex  items-center justify-center text-[10px] gap-6">
-        <p className="text-[#000] pr-2">16º</p>
-        <p className="text-[#000] pr-2 opacity-75">4º</p>
+        <p className="text-[#000] pr-2">{max}º</p>
+        <p className="text-[#000] pr-2 opacity-75">{min}º</p>
       </div>
     </div>
   );
--- src/components/body/index.tsx
+++ src/components/body/index.tsx
@@ -1,35 +1,67 @@
-import { IoCloudyOutline } from "react-icons/io5";
 import CardDetails from "../card";
 import WeekTemp from "../WeekTemp";
+import { useWheater } from "../../hooks/useWheather";
+
+const dataWeek = [
+  { id: 1, day: "Seg", dayNumber: 16, max: 30, min: 20 },
+  { id: 2, day: "Ter", dayNumber: 17, max: 28, min: 18 },
+  { id: 3, day: "Qua", dayNumber: 18, max: 26, min: 17 },
+  { id: 4, day: "Qui", dayNumber: 19, max: 25, min: 16 },
+  { id: 5, day: "Sex", dayNumber: 20, max: 27, min: 19 },
+  { id: 6, day: "Sáb", dayNumber: 21, max: 29, min: 21 },
+  { id: 7, day: "Dom", dayNumber: 22, max: 31, min: 22 },
+];
 
 export default function Body() {
+  const { data, isLoading, error } = useWheater("Paris");
+  const timestamp = data?.location.localtime_epoch;
+
+  const date = timestamp ? new Date(timestamp) : null;
+  console.log("a data", date);
+  console.log("meu data", data);
+  if (isLoading) return <p>Carregando...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <div className="h-full bg-gradient-to-r from-[#E8F7FF] to-[#BEDBFD] p-4">
-      {/* Grid adaptável: 1 coluna no mobile, 2 colunas a partir do md */}
       <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4 max-w-6xl mx-auto">
-        
-        {/* Coluna esquerda */}
         <div className="bg-white flex flex-col p-4 rounded shadow">
           <span className="opacity-60 text-sm">Agora em</span>
-          <span className="font-black text-lg sm:text-xl">São Paulo, SP</span>
+          <span className="font-black text-lg sm:text-xl">
+            {data?.location.name}, {data?.location.country}
+          </span>
 
           <div className="flex flex-col mt-2">
             <div className="flex items-center gap-2 text-[#81BDFC] text-2xl sm:text-3xl">
-              <IoCloudyOutline />
-              <span>24º</span>
+              <img src={data?.current.condition.icon} />
+              <span>{data?.current.temp_c}º</span>
             </div>
             <span className="pl-8 text-[10px] sm:text-xs text-[#D9D9D9] pb-2">
-              Parcialmente Nublado
+              {data?.current.condition.text}
             </span>
           </div>
 
           {/* Cards */}
           <div className="grid grid-cols-2 gap-2 mt-4">
-            <CardDetails />
-            <CardDetails />
-            <CardDetails />
-            <CardDetails />
-            <CardDetails />
+            <CardDetails
+              clima_info="Umidade"
+              clima_dados={data?.current?.humidity}
+            />
+            <CardDetails
+              clima_info="Sensação"
+              clima_dados={data?.current?.feelslike_c}
+            />
+            <CardDetails
+              clima_info="Vento"
+              clima_dados={data?.current?.wind_mph}
+            />
+            <CardDetails
+              clima_info="Direção do vento"
+              clima_dados={data?.current?.wind_dir}
+            />
+            <CardDetails
+              clima_info="Horário"
+              clima_dados={date?.toLocaleTimeString()}
+            />
           </div>
         </div>
 
@@ -37,16 +69,17 @@ export default function Body() {
         <div className="flex flex-col gap-2">
           <span className="text-sm sm:text-base">Próximos 7 dias</span>
           <div className="flex flex-col gap-1">
-            <WeekTemp />
-            <WeekTemp />
-            <WeekTemp />
-            <WeekTemp />
-            <WeekTemp />
-            <WeekTemp />
-            <WeekTemp />
+            {dataWeek.map((temp) => (
+              <WeekTemp
+                key={temp.id}
+                day={temp.day}
+                dayNumber={temp.dayNumber}
+                min={temp.min}
+                max={temp.max}
+              />
+            ))}
           </div>
         </div>
-        
       </div>
     </div>
   );
